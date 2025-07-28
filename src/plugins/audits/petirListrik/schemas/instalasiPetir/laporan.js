3918536a21c6d58b4d404dca46473cf7f3a6d5f9
ivalhad <ivan14hadrian@gmail.com>
2025-07-28 16:20:12 +0700

    update skemajoi petir

--- src/plugins/audits/petirListrik/schemas/instalasiPetir/laporan.js
+++ src/plugins/audits/petirListrik/schemas/instalasiPetir/laporan.js
@@ -3,98 +3,98 @@
 const Joi = require('joi');
 
 const dynamicTestItemSchema = Joi.object({
-    ecResult: Joi.string().allow('').optional(),
-    epResult: Joi.string().allow('').optional(),
-    rValue: Joi.number().allow(null).optional(),
-    result: Joi.string().allow('').optional()
+    ecResult: Joi.string().allow('').required(),
+    epResult: Joi.string().allow('').required(),
+    rValue: Joi.string().allow('').required(),
+    result: Joi.string().allow('').required()
 });
 
 const dynamicItemSchema = Joi.object({
-    materialConditionsItems: Joi.string().allow('').optional(),
-    outcomeBaik: Joi.boolean().allow(null).optional(),
-    outcomeBuruk: Joi.boolean().allow(null).optional(),
-    rGradeItems: Joi.string().allow('').optional(),
-    result: Joi.string().allow('').optional()
+    materialConditionsItems: Joi.string().allow('').required(),
+    outcomeBaik: Joi.boolean().allow(true, false).required(),
+    outcomeBuruk: Joi.boolean().allow(true, false).required(),
+    rGradeItems: Joi.string().allow('').required(),
+    result: Joi.string().allow('').required()
 });
 
 const laporanPetirPayload = Joi.object({
-    createdAt: Joi.string().allow('').optional(), 
-    examinationType: Joi.string().allow('').optional(),
-    equipmentType: Joi.string().allow('').optional(),
-    inspectionType: Joi.string().allow('').optional(),
-    extraId: Joi.alternatives().try(Joi.number(), Joi.string().valid('')).optional(),
+    createdAt: Joi.string().allow('').required(), 
+    examinationType: Joi.string().allow('').required(),
+    equipmentType: Joi.string().allow('').required(),
+    inspectionType: Joi.string().allow('').required(),
+    extraId: Joi.number().required(),
 
     pjk3Data: Joi.object({
-        companyNameandaddres: Joi.string().allow('').optional(),
-        companyPermitNo: Joi.string().allow('').optional(),
-        expertPermitNo: Joi.string().allow('').optional(),
-        expertName: Joi.string().allow('').optional(),
-        riksaujiTools: Joi.string().allow('').optional()
-    }).optional(),
+        companyNameandaddres: Joi.string().allow('').required(),
+        companyPermitNo: Joi.string().allow('').required(),
+        expertPermitNo: Joi.string().allow('').required(),
+        expertName: Joi.string().allow('').required(),
+        riksaujiTools: Joi.string().allow('').required()
+    }).required(),
 
     ownerData: Joi.object({
-        companyName: Joi.string().allow('').optional(),
-        companyLocation: Joi.string().allow('').optional(),
-        usageLocation: Joi.string().allow('').optional(),
-        objectType: Joi.string().allow('').optional(),
-        typeInspection: Joi.string().allow('').optional(),
-        certificateNo: Joi.string().allow('').optional(),
-        inspectionDate: Joi.string().allow('').optional()
-    }).optional(),
+        companyName: Joi.string().allow('').required(),
+        companyLocation: Joi.string().allow('').required(),
+        usageLocation: Joi.string().allow('').required(),
+        objectType: Joi.string().allow('').required(),
+        typeInspection: Joi.string().allow('').required(),
+        certificateNo: Joi.string().allow('').required(),
+        inspectionDate: Joi.string().allow('').required()
+    }).required(),
 
     technicalData: Joi.object({
-        conductorType: Joi.string().allow('').optional(),
-        buildingHeight: Joi.string().allow('').optional(),
-        buildingArea: Joi.string().allow('').optional(),
-        receiverHeight: Joi.string().allow('').optional(),
-        receiverCount: Joi.number().allow('').optional(),
-        testJointCount: Joi.number().allow('').optional(),
-        conductorDescription: Joi.string().allow('').optional(),
-        groundingResistance: Joi.string().allow('').optional(),
-        spreadingResistance: Joi.string().allow('').optional(),
-        installer: Joi.string().allow('').optional()
-    }).optional(),
+        conductorType: Joi.string().allow('').required(),
+        buildingHeight: Joi.string().allow('').required(),
+        buildingArea: Joi.string().allow('').required(),
+        receiverHeight: Joi.string().allow('').required(),
+        receiverCount: Joi.string().allow('').required(),
+        testJointCount: Joi.string().allow('').required(),
+        conductorDescription: Joi.string().allow('').required(),
+        groundingResistance: Joi.string().allow('').required(),
+        spreadingResistance: Joi.string().allow('').required(),
+        installer: Joi.string().allow('').required()
+    }).required(),
 
     physicalInspection: Joi.object({
-        installationSystem: Joi.object({ good: Joi.boolean(), fair: Joi.boolean(), poor: Joi.boolean() }).optional(),
-        receiverHead: Joi.object({ good: Joi.boolean(), fair: Joi.boolean(), poor: Joi.boolean() }).optional(),
-        receiverPole: Joi.object({ good: Joi.boolean(), fair: Joi.boolean(), poor: Joi.boolean() }).optional(),
-        poleReinforcement: Joi.object({ good: Joi.boolean(), fair: Joi.boolean(), poor: Joi.boolean() }).optional(),
-        downConductor: Joi.object({ good: Joi.boolean(), fair: Joi.boolean(), poor: Joi.boolean() }).optional(),
-        conductorClamps: Joi.object({ good: Joi.boolean(), fair: Joi.boolean(), poor: Joi.boolean() }).optional(),
-        jointConnections: Joi.object({ good: Joi.boolean(), fair: Joi.boolean(), poor: Joi.boolean() }).optional(),
-        groundingTerminalBox: Joi.object({ good: Joi.boolean(), fair: Joi.boolean(), poor: Joi.boolean() }).optional(),
-        controlBox: Joi.object({ good: Joi.boolean(), fair: Joi.boolean(), poor: Joi.boolean() }).optional(),
-        groundingSystem: Joi.object({ good: Joi.boolean(), fair: Joi.boolean(), poor: Joi.boolean() }).optional(),
-        conductorToGroundConnection: Joi.object({ good: Joi.boolean(), fair: Joi.boolean(), poor: Joi.boolean() }).optional(),
-        notes: Joi.string().allow('').optional()
-    }).optional(),
+        installationSystem: Joi.object({ good: Joi.boolean().allow(true, false), fair: Joi.boolean().allow(true, false), poor: Joi.boolean().allow(true, false) }).required(),
+        receiverHead: Joi.object({ good: Joi.boolean().allow(true, false), fair: Joi.boolean().allow(true, false), poor: Joi.boolean().allow(true, false) }).required(),
+        receiverPole: Joi.object({ good: Joi.boolean().allow(true, false), fair: Joi.boolean().allow(true, false), poor: Joi.boolean().allow(true, false) }).required(),
+        poleReinforcement: Joi.object({ good: Joi.boolean().allow(true, false), fair: Joi.boolean().allow(true, false), poor: Joi.boolean().allow(true, false) }).required(),
+        downConductor: Joi.object({ good: Joi.boolean().allow(true, false), fair: Joi.boolean().allow(true, false), poor: Joi.boolean().allow(true, false) }).required(),
+        conductorClamps: Joi.object({ good: Joi.boolean().allow(true, false), fair: Joi.boolean().allow(true, false), poor: Joi.boolean().allow(true, false) }).required(),
+        jointConnections: Joi.object({ good: Joi.boolean().allow(true, false), fair: Joi.boolean().allow(true, false), poor: Joi.boolean().allow(true, false) }).required(),
+        groundingTerminalBox: Joi.object({ good: Joi.boolean().allow(true, false), fair: Joi.boolean().allow(true, false), poor: Joi.boolean().allow(true, false) }).required(),
+        controlBox: Joi.object({ good: Joi.boolean().allow(true, false), fair: Joi.boolean().allow(true, false), poor: Joi.boolean().allow(true, false) }).required(),
+        groundingSystem: Joi.object({ good: Joi.boolean().allow(true, false), fair: Joi.boolean().allow(true, false), poor: Joi.boolean().allow(true, false) }).required(),
+        conductorToGroundConnection: Joi.object({ good: Joi.boolean().allow(true, false), fair: Joi.boolean().allow(true, false), poor: Joi.boolean().allow(true, false) }).required(),
+        notes: Joi.string().allow('').required()
+    }).required(),
 
     visualInspection: Joi.object({
-        airTerminal: Joi.string().allow('').optional(),
-        downConductorCheck: Joi.string().allow('').optional(),
-        groundingAndTestJoint: Joi.string().allow('').optional()
-    }).optional(),
+        airTerminal: Joi.string().allow('').required(),
+        downConductorCheck: Joi.string().allow('').required(),
+        groundingAndTestJoint: Joi.string().allow('').required()
+    }).required(),
 
     standardCompliance: Joi.object({
-        asBuiltDrawing: Joi.object({ good: Joi.boolean(), poor: Joi.boolean(), notes: Joi.string().allow('') }).optional(),
-        terminalToConductorConnection: Joi.object({ good: Joi.boolean(), poor: Joi.boolean(), notes: Joi.string().allow('') }).optional(),
-        downConductorJoints: Joi.object({ good: Joi.boolean(), poor: Joi.boolean(), notes: Joi.string().allow('') }).optional(),
-        testJointBoxInstallation: Joi.object({ good: Joi.boolean(), poor: Joi.boolean(), notes: Joi.string().allow('') }).optional(),
-        conductorMaterialStandard: Joi.object({ good: Joi.boolean(), poor: Joi.boolean(), notes: Joi.string().allow('') }).optional(),
-        lightningCounter: Joi.object({ good: Joi.boolean(), poor: Joi.boolean(), notes: Joi.string().allow('') }).optional(),
-        radioactiveTerminal: Joi.object({ good: Joi.boolean(), poor: Joi.boolean(), notes: Joi.string().allow('') }).optional(),
-        groundingRodMaterial: Joi.object({ good: Joi.boolean(), poor: Joi.boolean(), notes: Joi.string().allow('') }).optional(),
-        arresterInstallation: Joi.object({ good: Joi.boolean(), poor: Joi.boolean(), notes: Joi.string().allow('') }).optional()
-    }).optional(),
+        asBuiltDrawing: Joi.object({ good: Joi.boolean().allow(true, false), poor: Joi.boolean().allow(true, false), notes: Joi.string().allow('') }).required(),
+        terminalToConductorConnection: Joi.object({ good: Joi.boolean().allow(true, false), poor: Joi.boolean().allow(true, false), notes: Joi.string().allow('') }).required(),
+        downConductorJoints: Joi.object({ good: Joi.boolean().allow(true, false), poor: Joi.boolean().allow(true, false), notes: Joi.string().allow('') }).required(),
+        testJointBoxInstallation: Joi.object({ good: Joi.boolean().allow(true, false), poor: Joi.boolean().allow(true, false), notes: Joi.string().allow('') }).required(),
+        conductorMaterialStandard: Joi.object({ good: Joi.boolean().allow(true, false), poor: Joi.boolean().allow(true, false), notes: Joi.string().allow('') }).required(),
+        lightningCounter: Joi.object({ good: Joi.boolean().allow(true, false), poor: Joi.boolean().allow(true, false), notes: Joi.string().allow('') }).required(),
+        radioactiveTerminal: Joi.object({ good: Joi.boolean().allow(true, false), poor: Joi.boolean().allow(true, false), notes: Joi.string().allow('') }).required(),
+        groundingRodMaterial: Joi.object({ good: Joi.boolean().allow(true, false), poor: Joi.boolean().allow(true, false), notes: Joi.string().allow('') }).required(),
+        arresterInstallation: Joi.object({ good: Joi.boolean().allow(true, false), poor: Joi.boolean().allow(true, false), notes: Joi.string().allow('') }).required()
+    }).required(),
 
-    dynamicTestItems: Joi.array().items(dynamicTestItemSchema).optional(),
-    dynamicItems: Joi.array().items(dynamicItemSchema).optional(),
+    dynamicTestItems: Joi.array().items(dynamicTestItemSchema).required(),
+    dynamicItems: Joi.array().items(dynamicItemSchema).required(),
 
-    conclusion: Joi.string().allow('').optional(),
-    recomendations: Joi.string().allow('').optional()
+    conclusion: Joi.string().allow('').required(),
+    recomendations: Joi.string().allow('').required()
 
-}).min(1).unknown(false); // <--- Aturan ketat tetap dipertahankan
+});
 
 module.exports = {
     laporanPetirPayload,
