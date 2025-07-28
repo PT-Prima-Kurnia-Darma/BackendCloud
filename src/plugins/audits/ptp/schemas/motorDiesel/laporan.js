cdccea5b8edc49690fecd220acabe2aa136b23f5
ivalhad <ivan14hadrian@gmail.com>
2025-07-28 15:38:20 +0700

    update skemajoi ptp motor diesel

--- src/plugins/audits/ptp/schemas/motorDiesel/laporan.js
+++ src/plugins/audits/ptp/schemas/motorDiesel/laporan.js
@@ -4,66 +4,66 @@ const Joi = require('joi');
 
 // Skema untuk item inspeksi yang berulang (status boolean dan teks hasil)
 const inspectionItemSchema = Joi.object({
-    status: Joi.boolean().allow(null),
-    result: Joi.string().allow('', null).optional()
+    status: Joi.boolean().allow(true, false),
+    result: Joi.string().allow('').required()
 });
 
 // Skema untuk hasil pengujian NDT dan Safety Device
 const testResultSchema = Joi.object({
-    remarks: Joi.string().allow('', null).optional(),
-    result: Joi.string().allow('', null).optional()
+    remarks: Joi.string().allow('').required(),
+    result: Joi.string().allow('').required()
 });
 
 const laporanPtpDieselPayload = Joi.object({
-    examinationType: Joi.string().allow('').optional(),
-    extraId: Joi.number().allow(null).optional(),
-    createdAt: Joi.string().allow('').optional(),
-    inspectionType: Joi.string().allow('').optional(),
+    examinationType: Joi.string().allow('').required(),
+    extraId: Joi.number().required(),
+    createdAt: Joi.string().allow('').required(),
+    inspectionType: Joi.string().allow('').required(),
 
     generalData: Joi.object({
-        companyName: Joi.string().allow('').optional(),
-        companyLocation: Joi.string().allow('').optional(),
-        userInCharge: Joi.string().allow('').optional(),
-        userAddressInCharge: Joi.string().allow('').optional(),
-        subcontractorPersonInCharge: Joi.string().allow('').optional(),
-        unitLocation: Joi.string().allow('').optional(),
-        equipmentType: Joi.string().allow('').optional(),
-        brandType: Joi.string().allow('').optional(),
-        serialNumberUnitNumber: Joi.string().allow('').optional(),
-        manufacturer: Joi.string().allow('').optional(),
-        locationAndYearOfManufacture: Joi.string().allow('').optional(),
-        capacityWorkingLoad: Joi.string().allow('').optional(),
-        intendedUse: Joi.string().allow('').optional(),
-        pjk3SkpNo: Joi.string().allow('').optional(),
-        ak3SkpNo: Joi.string().allow('').optional(),
-        portableOrStationer: Joi.string().allow('').optional(),
-        usagePermitNumber: Joi.string().allow('').optional(),
-        operatorName: Joi.string().allow('').optional(),
-        equipmentHistory: Joi.string().allow('').optional()
-    }).optional(),
+        companyName: Joi.string().allow('').required(),
+        companyLocation: Joi.string().allow('').required(),
+        userInCharge: Joi.string().allow('').required(),
+        userAddressInCharge: Joi.string().allow('').required(),
+        subcontractorPersonInCharge: Joi.string().allow('').required(),
+        unitLocation: Joi.string().allow('').required(),
+        equipmentType: Joi.string().allow('').required(),
+        brandType: Joi.string().allow('').required(),
+        serialNumberUnitNumber: Joi.string().allow('').required(),
+        manufacturer: Joi.string().allow('').required(),
+        locationAndYearOfManufacture: Joi.string().allow('').required(),
+        capacityWorkingLoad: Joi.string().allow('').required(),
+        intendedUse: Joi.string().allow('').required(),
+        pjk3SkpNo: Joi.string().allow('').required(),
+        ak3SkpNo: Joi.string().allow('').required(),
+        portableOrStationer: Joi.string().allow('').required(),
+        usagePermitNumber: Joi.string().allow('').required(),
+        operatorName: Joi.string().allow('').required(),
+        equipmentHistory: Joi.string().allow('').required()
+    }).required(),
 
     technicalData: Joi.object({
         dieselMotor: Joi.object({
-            brandModel: Joi.string().allow('').optional(),
-            manufacturer: Joi.string().allow('').optional(),
-            classification: Joi.string().allow('').optional(),
-            serialNumber: Joi.string().allow('').optional(),
-            powerRpm: Joi.string().allow('').optional(),
-            startingPower: Joi.string().allow('').optional(),
-            cylinderCount: Joi.string().allow('').optional()
-        }).optional(),
+            brandModel: Joi.string().allow('').required(),
+            manufacturer: Joi.string().allow('').required(),
+            classification: Joi.string().allow('').required(),
+            serialNumber: Joi.string().allow('').required(),
+            powerRpm: Joi.string().allow('').required(),
+            startingPower: Joi.string().allow('').required(),
+            cylinderCount: Joi.string().allow('').required()
+        }).required(),
         generator: Joi.object({
-            brandType: Joi.string().allow('').optional(),
-            manufacturer: Joi.string().allow('').optional(),
-            serialNumber: Joi.string().allow('').optional(),
-            power: Joi.string().allow('').optional(),
-            frequency: Joi.string().allow('').optional(),
-            rpm: Joi.string().allow('').optional(),
-            voltage: Joi.string().allow('').optional(),
-            powerFactor: Joi.string().allow('').optional(),
-            current: Joi.string().allow('').optional()
-        }).optional()
-    }).optional(),
+            brandType: Joi.string().allow('').required(),
+            manufacturer: Joi.string().allow('').required(),
+            serialNumber: Joi.string().allow('').required(),
+            power: Joi.string().allow('').required(),
+            frequency: Joi.string().allow('').required(),
+            rpm: Joi.string().allow('').required(),
+            voltage: Joi.string().allow('').required(),
+            powerFactor: Joi.string().allow('').required(),
+            current: Joi.string().allow('').required()
+        }).required()
+    }).required(),
 
     visualChecks: Joi.object({
         basicConstruction: Joi.object({
@@ -71,13 +71,13 @@ const laporanPtpDieselPayload = Joi.object({
             dieselHousing: inspectionItemSchema,
             support: inspectionItemSchema,
             anchorBolt: inspectionItemSchema
-        }).optional(),
+        }).required(),
         structuralConstruction: Joi.object({
             dailyTank: inspectionItemSchema,
             muffler: inspectionItemSchema,
             airVessel: inspectionItemSchema,
             panel: inspectionItemSchema
-        }).optional(),
+        }).required(),
         lubricationSystem: Joi.object({
             oil: inspectionItemSchema,
             oilStrainer: inspectionItemSchema,
@@ -86,7 +86,7 @@ const laporanPtpDieselPayload = Joi.object({
             byPassFilter: inspectionItemSchema,
             safetyValve: inspectionItemSchema,
             packing: inspectionItemSchema
-        }).optional(),
+        }).required(),
         fuelSystem: Joi.object({
             dailyTank: inspectionItemSchema,
             fuelInjector: inspectionItemSchema,
@@ -99,7 +99,7 @@ const laporanPtpDieselPayload = Joi.object({
             throttleShaft: inspectionItemSchema,
             regulator: inspectionItemSchema,
             shutOffValve: inspectionItemSchema
-        }).optional(),
+        }).required(),
         startingAid: Joi.object({
             feedPump: inspectionItemSchema,
             fuelValve: inspectionItemSchema,
@@ -119,7 +119,7 @@ const laporanPtpDieselPayload = Joi.object({
             serviceMeter: inspectionItemSchema,
             tempSensor: inspectionItemSchema,
             startingMotor: inspectionItemSchema
-        }).optional(),
+        }).required(),
         coolingSystem: Joi.object({
             coolingWater: inspectionItemSchema,
             bolts: inspectionItemSchema,
@@ -130,7 +130,7 @@ const laporanPtpDieselPayload = Joi.object({
             fanGuard: inspectionItemSchema,
             fanRotation: inspectionItemSchema,
             bearing: inspectionItemSchema
-        }).optional(),
+        }).required(),
         airCirculationSystem: Joi.object({
             preCleaner: inspectionItemSchema,
             dustIndicator: inspectionItemSchema,
@@ -142,7 +142,7 @@ const laporanPtpDieselPayload = Joi.object({
             silencer: inspectionItemSchema,
             heatDamper: inspectionItemSchema,
             bolts: inspectionItemSchema
-        }).optional(),
+        }).required(),
         mainParts: Joi.object({
             damperBolts: inspectionItemSchema,
             support: inspectionItemSchema,
@@ -151,7 +151,7 @@ const laporanPtpDieselPayload = Joi.object({
             vibrationDamper: inspectionItemSchema,
             beltAndPulley: inspectionItemSchema,
             crankshaft: inspectionItemSchema
-        }).optional(),
+        }).required(),
         generatorParts: Joi.object({
             terminal: inspectionItemSchema,
             cable: inspectionItemSchema,
@@ -161,19 +161,19 @@ const laporanPtpDieselPayload = Joi.object({
             frequencyMeter: inspectionItemSchema,
             circuitBreaker: inspectionItemSchema,
             onOffSwitch: inspectionItemSchema
-        }).optional(),
+        }).required(),
         transmission: Joi.object({
             gear: inspectionItemSchema,
             belt: inspectionItemSchema,
             chain: inspectionItemSchema
-        }).optional(),
+        }).required(),
         mdp: Joi.object({
             cable: inspectionItemSchema,
             condition: inspectionItemSchema,
             ampereMeter: inspectionItemSchema,
             voltMeter: inspectionItemSchema,
             mainCircuitBreaker: inspectionItemSchema
-        }).optional(),
+        }).required(),
         safetyDevice: Joi.object({
             grounding: inspectionItemSchema,
             lightningArrester: inspectionItemSchema,
@@ -186,8 +186,8 @@ const laporanPtpDieselPayload = Joi.object({
             vibrationDamper: inspectionItemSchema,
             circuitBreaker: inspectionItemSchema,
             avr: inspectionItemSchema
-        }).optional()
-    }).optional(),
+        }).required()
+    }).required(),
 
     tests: Joi.object({
         ndt: Joi.object({
@@ -196,7 +196,7 @@ const laporanPtpDieselPayload = Joi.object({
             noise: testResultSchema,
             lighting: testResultSchema,
             loadTest: testResultSchema
-        }).optional(),
+        }).required(),
         safetyDevice: Joi.object({
             governor: testResultSchema,
             emergencyStop: testResultSchema,
@@ -207,62 +207,62 @@ const laporanPtpDieselPayload = Joi.object({
             waterIndicator: testResultSchema,
             safetyValves: testResultSchema,
             radiator: testResultSchema
-        }).optional()
-    }).optional(),
+        }).required()
+    }).required(),
 
     electricalComponents: Joi.object({
         panelControl: Joi.object({
-            ka: Joi.string().allow('').optional(),
+            ka: Joi.string().allow('').required(),
             voltage: Joi.object({
-                rs: Joi.number().allow(null).optional(),
-                rt: Joi.number().allow(null).optional(),
-                st: Joi.number().allow(null).optional(),
-                rn: Joi.number().allow(null).optional(),
-                rg: Joi.number().allow(null).optional(),
-                ng: Joi.number().allow(null).optional()
-            }).optional(),
+                rs: Joi.string().allow('').required(),
+                rt: Joi.string().allow('').required(),
+                st: Joi.string().allow('').required(),
+                rn: Joi.string().allow('').required(),
+                rg: Joi.string().allow('').required(),
+                ng: Joi.string().allow('').required()
+            }).required(),
             powerInfo: Joi.object({
-                frequency: Joi.string().allow('').optional(),
-                cosQ: Joi.number().allow(null).optional(),
+                frequency: Joi.string().allow('').required(),
+                cosQ: Joi.string().allow('').required(),
                 ampere: Joi.object({
-                    r: Joi.number().allow(null).optional(),
-                    s: Joi.number().allow(null).optional(),
-                    t: Joi.number().allow(null).optional()
-                }).optional(),
-                result: Joi.string().allow('').optional()
-            }).optional()
-        }).optional()
-    }).optional(),
+                    r: Joi.string().allow('').required(),
+                    s: Joi.string().allow('').required(),
+                    t: Joi.string().allow('').required()
+                }).required(),
+                result: Joi.string().allow('').required()
+            }).required()
+        }).required()
+    }).required(),
 
-    conclusion: Joi.string().allow('').optional(),
-    recommendations: Joi.string().allow('').optional(),
-    inspectionDate: Joi.string().allow('').optional(),
+    conclusion: Joi.string().allow('').required(),
+    recommendations: Joi.string().allow('').required(),
+    inspectionDate: Joi.string().allow('').required(),
 
     mcbCalculation: Joi.object({
-        phase: Joi.number().allow(null).optional(),
-        voltage: Joi.string().allow('').optional(),
-        cosQ: Joi.number().allow(null).optional(),
-        generatorPowerKva: Joi.number().allow(null).optional(),
-        generatorPowerKw: Joi.number().allow(null).optional(),
-        resultCalculation: Joi.number().allow(null).optional(),
-        requirementCalculation: Joi.number().allow(null).optional(),
-        conclusion: Joi.string().allow('').optional()
-    }).optional(),
+        phase: Joi.string().allow('').required(),
+        voltage: Joi.string().allow('').required(),
+        cosQ: Joi.string().allow('').required(),
+        generatorPowerKva: Joi.string().allow('').required(),
+        generatorPowerKw: Joi.string().allow('').required(),
+        resultCalculation: Joi.string().allow('').required(),
+        requirementCalculation: Joi.string().allow('').required(),
+        conclusion: Joi.string().allow('').required()
+    }).required(),
 
     noiseMeasurement: Joi.object({
-        pointA: Joi.object({ result: Joi.number().allow(null).optional(), status: Joi.string().allow('').optional() }).optional(),
-        pointB: Joi.object({ result: Joi.number().allow(null).optional(), status: Joi.string().allow('').optional() }).optional(),
-        pointC: Joi.object({ result: Joi.number().allow(null).optional(), status: Joi.string().allow('').optional() }).optional(),
-        pointD: Joi.object({ result: Joi.number().allow(null).optional(), status: Joi.string().allow('').optional() }).optional()
-    }).optional(),
+        pointA: Joi.object({ result: Joi.string().allow('').required(), status: Joi.string().allow('').required() }).required(),
+        pointB: Joi.object({ result: Joi.string().allow('').required(), status: Joi.string().allow('').required() }).required(),
+        pointC: Joi.object({ result: Joi.string().allow('').required(), status: Joi.string().allow('').required() }).required(),
+        pointD: Joi.object({ result: Joi.string().allow('').required(), status: Joi.string().allow('').required() }).required()
+    }).required(),
 
     lightingMeasurement: Joi.object({
-        pointA: Joi.object({ result: Joi.number().allow(null).optional(), status: Joi.string().allow('').optional() }).optional(),
-        pointB: Joi.object({ result: Joi.number().allow(null).optional(), status: Joi.string().allow('').optional() }).optional(),
-        pointC: Joi.object({ result: Joi.number().allow(null).optional(), status: Joi.string().allow('').optional() }).optional(),
-        pointD: Joi.object({ result: Joi.number().allow(null).optional(), status: Joi.string().allow('').optional() }).optional()
-    }).optional()
-}).min(1).unknown(false);
+        pointA: Joi.object({ result: Joi.string().allow('').required(), status: Joi.string().allow('').required() }).required(),
+        pointB: Joi.object({ result: Joi.string().allow('').required(), status: Joi.string().allow('').required() }).required(),
+        pointC: Joi.object({ result: Joi.string().allow('').required(), status: Joi.string().allow('').required() }).required(),
+        pointD: Joi.object({ result: Joi.string().allow('').required(), status: Joi.string().allow('').required() }).required()
+    }).required()
+});
 
 module.exports = {
     laporanPtpDieselPayload
