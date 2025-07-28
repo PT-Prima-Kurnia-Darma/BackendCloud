b77918775f285cb9ba4d5d2f1571d0950cc9415e
ivalhad <ivan14hadrian@gmail.com>
2025-07-28 14:46:19 +0700

    update skema joi PUBT

--- src/plugins/audits/pubt/schemas/bap.js
+++ src/plugins/audits/pubt/schemas/bap.js
@@ -4,53 +4,53 @@ const Joi = require('joi');
 
 const bapPubtPayload = Joi.object({
     laporanId: Joi.string().required(),
-    examinationType: Joi.string().allow('').optional(),
-    inspectionType: Joi.string().allow('').optional(),
-    inspectionDate: Joi.string().allow('').optional(),
-    createdAt: Joi.string().allow('').optional(),
-    equipmentType: Joi.string().allow('').optional(),
-    extraId: Joi.number().allow(null, '').optional(),
+    examinationType: Joi.string().allow('').required(),
+    inspectionType: Joi.string().allow('').required(),
+    inspectionDate: Joi.string().allow('').required(),
+    createdAt: Joi.string().allow('').required(),
+    equipmentType: Joi.string().allow('').required(),
+    extraId: Joi.number().required(),
 
     generalData: Joi.object({
-        companyName: Joi.string().allow('').optional(),
-        companyLocation: Joi.string().allow('').optional(),
-        userUsage: Joi.string().allow('').optional(),
-        userAddress: Joi.string().allow('').optional()
-    }).optional(),
+        companyName: Joi.string().allow('').required(),
+        companyLocation: Joi.string().allow('').required(),
+        userUsage: Joi.string().allow('').required(),
+        userAddress: Joi.string().allow('').required()
+    }).required(),
 
     technicalData: Joi.object({
-        brandType: Joi.string().allow('').optional(),
-        manufacturer: Joi.string().allow('').optional(),
-        countryAndYearOfManufacture: Joi.string().allow('').optional(),
-        serialNumberUnitNumber: Joi.string().allow('').optional(),
-        fuelType: Joi.string().allow('').optional(),
-        operatingPressure: Joi.string().allow('').optional(),
-        designPressureKgCm2: Joi.number().allow(null).optional(),
-        maxAllowableWorkingPressure: Joi.number().allow(null).optional(),
-        technicalDataShellMaterial: Joi.string().allow('').optional(),
-        safetyValveType: Joi.string().allow('').optional(),
-        volumeLiters: Joi.number().allow(null).optional()
-    }).optional(),
+        brandType: Joi.string().allow('').required(),
+        manufacturer: Joi.string().allow('').required(),
+        countryAndYearOfManufacture: Joi.string().allow('').required(),
+        serialNumberUnitNumber: Joi.string().allow('').required(),
+        fuelType: Joi.string().allow('').required(),
+        operatingPressure: Joi.string().allow('').required(),
+        designPressureKgCm2: Joi.string().allow('').required(),
+        maxAllowableWorkingPressure: Joi.string().allow('').required(),
+        technicalDataShellMaterial: Joi.string().allow('').required(),
+        safetyValveType: Joi.string().allow('').required(),
+        volumeLiters: Joi.string().allow('').required()
+    }).required(),
 
     visualInspection: Joi.object({
-        foundationCondition: Joi.boolean().allow(null).optional(),
-        safetyValveIsInstalled: Joi.boolean().allow(null).optional(),
-        safetyValveCondition: Joi.boolean().allow(null).optional(),
-        aparAvailable: Joi.boolean().allow(null).optional(),
-        aparCondition: Joi.boolean().allow(null).optional(),
-        wheelCondition: Joi.boolean().allow(null).optional(),
-        pipeCondition: Joi.boolean().allow(null).optional()
-    }).optional(),
+        foundationCondition: Joi.boolean().allow(true, false).required(),
+        safetyValveIsInstalled: Joi.boolean().allow(true, false).required(),
+        safetyValveCondition: Joi.boolean().allow(true, false).required(),
+        aparAvailable: Joi.boolean().allow(true, false).required(),
+        aparCondition: Joi.boolean().allow(true, false).required(),
+        wheelCondition: Joi.boolean().allow(true, false).required(),
+        pipeCondition: Joi.boolean().allow(true, false).required()
+    }).required(),
 
     testing: Joi.object({
-        ndtTestingFulfilled: Joi.boolean().allow(null).optional(),
-        thicknessTestingComply: Joi.boolean().allow(null).optional(),
-        pneumaticTestingCondition: Joi.boolean().allow(null).optional(),
-        hydroTestingFullFilled: Joi.boolean().allow(null).optional(),
-        safetyValveTestingCondition: Joi.boolean().allow(null).optional()
-    }).optional()
+        ndtTestingFulfilled: Joi.boolean().allow(true, false).required(),
+        thicknessTestingComply: Joi.boolean().allow(true, false).required(),
+        pneumaticTestingCondition: Joi.boolean().allow(true, false).required(),
+        hydroTestingFullFilled: Joi.boolean().allow(true, false).required(),
+        safetyValveTestingCondition: Joi.boolean().allow(true, false).required()
+    }).required()
 
-}).min(1).unknown(false);
+});
 
 module.exports = {
     bapPubtPayload,
